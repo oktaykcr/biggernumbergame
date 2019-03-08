2f7cd0a8c282aff3fdcc4ffc1481efea99923576
Oktay Kocer <oktaykocer@outlook.com>
2019-03-09 00:43:47 +0300

    fixed high_score file read operation crash problem when the game first time use

--- app/src/main/java/com/example/biggernumbergame/services/StorageService.java
+++ app/src/main/java/com/example/biggernumbergame/services/StorageService.java
@@ -3,6 +3,7 @@ package com.example.biggernumbergame.services;
 import android.content.Context;
 import android.util.Log;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Scanner;
@@ -10,22 +11,27 @@ import java.util.Scanner;
 public class StorageService {
 
     public StringBuilder readFile(Context context, String fileName) {
-        StringBuilder builder = new StringBuilder();
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(context.openFileInput(fileName));
-            while (scanner.hasNext()) {
-                builder.append(scanner.nextLine());
-                builder.append("\n");
+        File file = new File(context.getFilesDir(), fileName);
+        if(file.exists()) {
+            StringBuilder builder = new StringBuilder();
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(context.openFileInput(fileName));
+                while (scanner.hasNext()) {
+                    builder.append(scanner.nextLine());
+                }
+                scanner.close();
+                Log.i("FILE", fileName + " reading successful");
+            } catch (FileNotFoundException e) {
+                Log.e("FILE", fileName + " could not read");
+                e.printStackTrace();
             }
-            scanner.close();
-            Log.i("FILE", fileName + " reading successful");
-        } catch (FileNotFoundException e) {
-            Log.e("FILE", fileName + " could not read");
-            e.printStackTrace();
-        }
 
-        return builder;
+            return builder;
+        } else {
+            writeFile(context, fileName, "0,0");
+        }
+        return new StringBuilder().append("0,0");
     }
 
     public void writeFile(Context context, String fileName, String fileContent) {
